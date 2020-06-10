c2a4d1f1c4510090a8bcb4c2f7a5ec106ae4f4e9
IH <mail4others91@gmail.com>
2020-06-10 11:52:26 +0530

    Created interfaces

---
+++ CodingTest/CodingTest/BusinessService/IMembership.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest.BusinessService
+{
+    public interface IMembership
+    {
+        void ActivateMembership();
+        void UpgradeMembership();
+        void SendMail();
+    }
+}
