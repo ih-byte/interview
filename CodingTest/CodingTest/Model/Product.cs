89efae542dae8389be310db677ef065c0d521ae0
IH <mail4others91@gmail.com>
2020-06-10 11:57:34 +0530

    Add product model

---
+++ CodingTest/CodingTest/Model/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest.Model
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public double ProductUnitPrice { get; set; }
+        public int ProductQuantity { get; set; }
+
+    }
+}
