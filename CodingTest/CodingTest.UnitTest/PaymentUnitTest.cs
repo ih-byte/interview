854f46df096925574f6134c9ef706a24a00c6407
IH <mail4others91@gmail.com>
2020-06-10 13:46:35 +0530

    Added Payment unit test cases

---
+++ CodingTest/CodingTest.UnitTest/PaymentUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CodingTest.BusinessService;
+using CodingTest.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingTest.UnitTest
+{
+    [TestClass]
+    public class PaymentUnitTest
+    {
+        [TestMethod]
+        public void TestCalculateAmount()
+        {
+            ProductItem item = new ProductItem
+            {
+                ProductId = 1,
+                ProductQuantity = 1,
+                ProductUnitPrice = 100.00
+            };
+            IPayment payment = new Payment();
+            var amount = payment.CalculateAmount(item);
+            Assert.AreEqual(100.00, amount);
+        }
+    }
+}
