7836664edfee98f02049486d5638f136766f3d40
IH <mail4others91@gmail.com>
2020-06-10 12:22:01 +0530

    Created product bussiness class

---
+++ CodingTest/CodingTest/BusinessService/Product.cs
@@ -0,0 +1,19 @@
+﻿using CodingTest.Model;
+using System.Windows.Forms;
+
+namespace CodingTest.BusinessService
+{
+    public class Product : IPayment, IPacking
+    {
+        public double CalculateAmount(ProductItem product)
+        {
+            MessageBox.Show("Calculate Total Amount");
+            return 0;
+        }
+
+        public void GenerateSlip()
+        {
+            MessageBox.Show("Slip generated");
+        }
+    }
+}
