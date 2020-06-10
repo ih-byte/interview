e6a0e13dbe5e32ea095a9cbfba7a38ed087a2e17
IH <mail4others91@gmail.com>
2020-06-10 13:42:53 +0530

    Added unit test for packing

---
+++ CodingTest/CodingTest.UnitTest/PackingUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CodingTest.BusinessService;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingTest.UnitTest
+{
+    [TestClass]
+    public class PackingUnitTest
+    {
+        [TestMethod]
+        public void TestGenerateSlip()
+        {
+            IPacking packing = new Packing();
+            packing.GenerateSlip();
+            Assert.IsTrue(true);
+        } 
+    }
+}
